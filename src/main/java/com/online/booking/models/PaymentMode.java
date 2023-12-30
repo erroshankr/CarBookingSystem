c04ce606457adbdea13c130087307f5aa19dc6f4
Vaibhav Jha <vaibhavjha521@gmail.com>
2023-12-30 22:01:40 +0530

    vaibhav_intial_commit

---
+++ src/main/java/com/online/booking/models/PaymentMode.java
@@ -0,0 +1,7 @@
+package com.online.booking.models;
+
+public  enum PaymentMode{
+        CASH,
+        CREDIT_CARD,
+        DEBIT_CARD,
+    }
---
+++ src/main/java/com/online/booking/models/PaymentStatus.java
@@ -0,0 +1,9 @@
+package com.online.booking.models;
+
+public enum PaymentStatus{
+        PENDING,
+        PROCESSING,
+        SUCCESS,
+        FAILURE
+
+    }
