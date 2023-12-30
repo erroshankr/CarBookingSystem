a571bbd71a3b03e9f8f413c00a986d871f24b459
spquadir <s.p.quadir555@gmail.com>
2023-12-31 00:16:24 +0530

    sneha-->user,driver(controller,service) color,location(repository)

--- src/main/java/com/online/booking/repo/ColorRepository.java
+++ src/main/java/com/online/booking/repo/ColorRepository.java
@@ -1,10 +1,9 @@
 package com.online.booking.repo;
 
-
-import com.online.booking.models.Location;
+import com.online.booking.models.Color;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LocationRepository extends CrudRepository<Location,String> {
+public interface ColorRepository extends CrudRepository<Color,String> {
 }
---
+++ src/main/java/com/online/booking/service/DriverController.java
@@ -0,0 +1,7 @@
+package com.online.booking.service;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class DriverController {
+}
