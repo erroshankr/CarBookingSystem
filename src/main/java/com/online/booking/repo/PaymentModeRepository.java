d222f44938385579f26df0a85c8a40e14677515c
Vaibhav Jha <vaibhavjha521@gmail.com>
2023-12-31 21:21:49 +0530

    vaibhav_new Commit_serviseand COntroller

--- src/main/java/com/online/booking/repo/PaymentModeRepository.java
+++ src/main/java/com/online/booking/repo/PaymentModeRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PaymentModeRepository extends CrudRepository<PaymentMode,Long>{
+public interface PaymentModeRepository extends CrudRepository<PaymentMode,String>{
 }
