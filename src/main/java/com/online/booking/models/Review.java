335c623fc30353c994796b3cb59d83cfcc81670f
BAPI <paswanbapi2001@gmail.com>
2023-12-31 12:03:13 +0530

    review committed

---
+++ src/main/java/com/online/booking/models/Review.java
@@ -0,0 +1,32 @@
+package com.online.booking.models;
+
+public class Review {
+    private int rating;
+    private String comment;
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public int getReviewId() {
+        return reviewId;
+    }
+
+    public void setReviewId(int reviewId) {
+        this.reviewId = reviewId;
+    }
+
+    private int reviewId;
+}
