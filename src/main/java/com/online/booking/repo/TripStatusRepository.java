476f063a876c6e3cf7e26297c1c9014a23318783
anupamgith <aj4954265@gmail.com>
2023-12-31 21:53:25 +0530

    anupam-vehicles

--- src/main/java/com/online/booking/repo/TripStatusRepository.java
+++ src/main/java/com/online/booking/repo/TripStatusRepository.java
@@ -2,6 +2,8 @@ package com.online.booking.repo;
 
 import com.online.booking.models.Trip;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TripStatusRepository extends CrudRepository<Trip,Integer> {
 }
--- src/main/java/com/online/booking/repo/UserRepository.java
+++ src/main/java/com/online/booking/repo/UserRepository.java
@@ -2,6 +2,8 @@ package com.online.booking.repo;
 
 import com.online.booking.models.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends CrudRepository<User,Integer> {
 }
