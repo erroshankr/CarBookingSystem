16a4e61c0d9f529176a0685e755142d195f898ac
Tushar166 <tusharbhaskar.1116@gmail.com>
2023-12-31 21:18:39 +0530

    tushar-Admin,CarType-service

--- src/main/java/com/online/booking/repo/AdminRepository.java
+++ src/main/java/com/online/booking/repo/AdminRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AdminRepository extends CrudRepository<Admin,String> {
+public interface AdminRepository extends CrudRepository<Admin,Integer> {
 }
--- src/main/java/com/online/booking/repo/CarTypeRepository.java
+++ src/main/java/com/online/booking/repo/CarTypeRepository.java
@@ -1,8 +1,9 @@
 package com.online.booking.repo;
 
+import com.online.booking.models.CarType;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CarType extends CrudRepository<CarType,String> {
+public interface CarTypeRepository extends CrudRepository<CarType,String> {
 }
