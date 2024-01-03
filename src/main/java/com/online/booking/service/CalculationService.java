b439f5b840495af2d10f98af9aa0f6b90799cb5b
Ashwani Mishra <ashwanimishra682002@gmail.com>
2024-01-03 18:52:37 +0530

    commited CalculationService

--- src/main/java/com/online/booking/service/CalculationService.java
+++ src/main/java/com/online/booking/service/CalculationService.java
@@ -1,12 +1,16 @@
 package com.online.booking.service;
 
+import com.online.booking.enums.CarType;
 import org.springframework.stereotype.Service;
 
 @Service
-public interface CalculationService {
-
+public interface CalculationService
+{
     double FARE_PER_KILOMETER_HATCHBACK = 8;
     double FARE_PER_KILOMETER_SEDAN = 10;
     double FARE_PER_KILOMETER_SUV = 12;
+
+    double calculateTotalFare(int sourceLocID, int destLocID, CarType type);
+    double findTotalEarning(int driverID);
 
 }
