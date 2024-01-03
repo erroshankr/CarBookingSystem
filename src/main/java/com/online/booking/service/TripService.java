9384fc1e795c1526c7400acc053982d1df57b5d2
spquadir <s.p.quadir555@gmail.com>
2024-01-03 20:51:30 +0530

    sneha--> crud in TripService

--- src/main/java/com/online/booking/service/TripService.java
+++ src/main/java/com/online/booking/service/TripService.java
@@ -1,7 +1,21 @@
 package com.online.booking.service;
 
+import com.online.booking.models.TripModel;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface TripService {
+
+    TripModel createTrip(TripModel trip);
+
+    boolean editTripByID(int tripID);
+
+    boolean deleteTripByID(int tripID);
+
+    TripModel fetchTripByID(int tripID);
+
+    List<TripModel> fetchAllTrips();
+
 }
---
+++ src/main/java/com/online/booking/service/impl/TripServiceImpl.java
@@ -0,0 +1,101 @@
+package com.online.booking.service.impl;
+
+import com.online.booking.models.TripModel;
+import com.online.booking.models.UserModel;
+import com.online.booking.repo.TripRepository;
+import com.online.booking.service.TripService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+
+public class TripServiceImpl implements TripService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(TripServiceImpl.class);
+    @Autowired
+    private TripRepository tripRepo;
+    @Autowired
+    private TripModel tripModel;
+
+
+    public  TripModel createTrip(TripModel trip){
+       TripModel tripModel1 = new TripModel();
+       tripModel1.setSource(trip.getSource());
+       tripModel1.setDestination(trip.getDestination());
+       tripModel1.setPickupTime(trip.getPickupTime());
+       tripModel1.setDropOffTime(trip.getDropOffTime());
+       tripModel1.setDistance(trip.getDistance());
+       tripModel1.setFare(trip.getFare());
+       tripModel1.setReview(trip.getReview());
+        tripRepo.save(tripModel1);
+        LOG.info("Created a new trip successfully");
+        return tripModel1;
+    }
+
+    public boolean editTripByID(int tripID){
+        Optional<TripModel> updateTrip = tripRepo.findById(tripModel.getTripID());
+        if(updateTrip.isEmpty()){
+            LOG.error("No trip found with tripID : " + tripModel.getTripID());
+            return false;
+        }
+        boolean result = false;
+        TripModel trip = updateTrip.get();
+
+            trip.setSource(tripModel.getSource());
+            trip.setDestination(tripModel.getDestination());
+            trip.setDistance(tripModel.getDistance());
+            trip.setPickupTime(tripModel.getPickupTime());
+            trip.setDropOffTime(tripModel.getDropOffTime());
+            trip.setReview(tripModel.getReview());
+            try {
+                tripRepo.save(trip);
+                LOG.info("Updated successfully");
+                result = true;
+            }catch(Exception e)
+            {
+                LOG.error("Exception occurred while saving trip with ID " + tripModel.getTripID());
+                result = false;
+            }
+
+        return result ;
+    }
+
+    public  boolean deleteTripByID(int tripID){
+        Optional<TripModel> deleteTrip = tripRepo.findById(tripModel.getTripID());
+        if(deleteTrip.isEmpty()){
+            LOG.error("No trip found with tripID : " + tripModel.getTripID());
+            return false;
+        }
+        boolean result = false;
+        TripModel trip = deleteTrip.get();
+          try{
+            tripRepo.deleteById(trip.getTripID());
+            LOG.info("Trip with " + trip.getTripID() + " deleted successfully");
+            result = true ;
+          }catch(Exception e){
+              LOG.error("Exception occurred while saving trip with ID " + trip.getTripID());
+              result = false;
+
+          }
+        return result ;
+    }
+
+    public TripModel fetchTripByID(int tripID){
+        TripModel findTrip = tripRepo.findById(tripModel.getTripID()).orElse(null);
+        if(findTrip != null)
+        {
+            LOG.info("Found Successfully " + findTrip.getTripID());
+            return findTrip;
+        }
+        return null;
+    }
+
+
+    public List<TripModel> fetchAllTrips(){
+        List<TripModel> findAll  = (List<TripModel>) tripRepo.findAll();
+        return findAll;
+
+    }
+}
