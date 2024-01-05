9ef6d1412d98f2ea97081d472f03b7621678fb88
erroshankr <roshankumarjha24@gmail.com>
2024-01-05 21:30:41 +0530

    roshan-adding method to findTotalEarningByDriver

--- src/main/java/com/online/booking/service/CalculationService.java
+++ src/main/java/com/online/booking/service/CalculationService.java
@@ -2,7 +2,6 @@ package com.online.booking.service;
 
 import com.online.booking.enums.CarType;
 import com.online.booking.exceptions.InvalidLocationException;
-import com.online.booking.exceptions.UserNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,6 +12,5 @@ public interface CalculationService
     double FARE_PER_KILOMETER_SUV = 12;
 
     double calculateTotalFare(int sourceLocID, int destLocID, CarType type) throws InvalidLocationException;
-    double findTotalEarning(int driverID) throws UserNotFoundException;
 
 }
